dae2a8b7f68fbe3ef8085bda08e8b0e59d9d5ddf
nemoedelo <elenakulagina.mail@gmail.com>
2020-07-19 11:20:55 +0300

    initial commit

--- src/test/java/com/example/geekbrainsdb/SumTest.java
+++ src/test/java/com/example/geekbrainsdb/SumTest.java
@@ -1,5 +1,6 @@
 package com.example.geekbrainsdb;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,19 +9,20 @@ import org.slf4j.LoggerFactory;
 
 import java.util.stream.Stream;
 
-public class LoggerTest {
+public class SumTest {
 
-    Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+    Logger logger = LoggerFactory.getLogger(SumTest.class);
 
 
     @ParameterizedTest
     @MethodSource("data")
-    public void testLogging(String first, Long second){
-        System.out.println(first + second);
+    public void testLogging(Long first, Long second, Long expected){
+        Assertions.assertEquals(expected, first + second);
     }
 
+
     private static Stream<Arguments> data(){
-        return Stream.of(Arguments.of("1", 2L), Arguments.of("2", 3L));
+        return Stream.of(Arguments.of(2L, 2L, 4L), Arguments.of(-3L, 3L, 0L));
 
     }
 
